94d4369858e699ec58b4abf781a9860069b3f54d
BeyondOrigins <dima.a.ivlev@gmail.com>
2023-01-27 18:29:50 +0300

    Done

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template
+from users import users
 app = Flask(__name__)
 
-@app.route('/flag')
-def index():
-    return render_template('index.html')
+
+class Worker:
+    def __init__(self, name, age, developmentExperience, image, education):
+        self.name = name
+        self.age = age
+        self.developmentExperience = developmentExperience
+        self.image = image
+        self.education = education
+
+
+"""
+    All IDs are in users.py
+    If can't find user with such an ID, renders cantFindUser.html
+"""
+
+@app.route("/users/<string:id>")
+def show_user_info(id):
+    try:
+        user = Worker(users[id]["name"],
+                      users[id]["age"],
+                      users[id]["developmentExperience"],
+                      users[id]["image"],
+                      users[id]["education"])
+        return render_template("index.html", user=user)
+    
+    except:
+        return render_template("cantFindUser.html")
 
 if __name__ == "__main__":
     app.run()
---
+++ users.py
@@ -0,0 +1,37 @@
+users = {
+    "1983423542" : {
+        "name" : "Кочаргин Михаил",
+        "age" : 28,
+        "developmentExperience" : 5,
+        "image" : "/static/images/image1.jpeg",
+        "education" : "школа программирования Coding Skills"
+    },
+    "9256837609" : {
+        "name" : "Костенко Владимир",
+        "age" : 31,
+        "developmentExperience" : 7,
+        "image" : "/static/images/image2.jpg",
+        "education" : "Яндекс Лицей"
+    },
+    "3867396738" : {
+        "name" : "Сосновцев Игорь",
+        "age" : 23,
+        "developmentExperience" : 4,
+        "image" : "/static/images/image3.jpg",
+        "education" : "Стэндфордский университет"
+    },
+    "7593758398" : {
+        "name" : "Волков Даниил",
+        "age" : 27,
+        "developmentExperience" : 4,
+        "image" : "/static/images/image4.jpeg",
+        "education" : "школа программирования Coding Skills"
+    },
+    "5748690473" : {
+        "name" : "Титова Екатерина",
+        "age" : 26,
+        "developmentExperience" : 3,
+        "image" : "/static/images/image5.jpg",
+        "education" : "университет ВШЭ"
+    }
+}
